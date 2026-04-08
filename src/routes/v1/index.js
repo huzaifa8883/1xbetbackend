a14e63773b72c6c89dbd7cadf3fd55a3e4a0d870
huzaifa8883 <aw3540341@gmail.com>
2026-04-08 21:16:59 +0500

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,19 +1,16 @@
 'use strict';
 
-const { Router } = require('express');
+const express = require('express');
+const router  = express.Router();
 
-const authRoutes        = require('./auth.routes');
-const userRoutes        = require('./user.routes');
-const orderRoutes       = require('./order.routes');
-const marketRoutes      = require('./market.routes');
-const sportConfigRoutes = require('./sportconfig.routes');
+const authRoutes    = require('./auth.routes');
+const orderRoutes   = require('./order.routes');
+const marketRoutes  = require('./market.routes');
+const userRoutes    = require('./user.routes');
 
-const router = Router();
-
-router.use('/auth',         authRoutes);
-router.use('/users',        userRoutes);
-router.use('/orders',       orderRoutes);
-router.use('/markets',      marketRoutes);
-router.use('/sport-config', sportConfigRoutes);
+router.use('/auth',    authRoutes);
+router.use('/orders',  orderRoutes);
+router.use('/markets', marketRoutes);
+router.use('/users',   userRoutes);
 
 module.exports = router;
