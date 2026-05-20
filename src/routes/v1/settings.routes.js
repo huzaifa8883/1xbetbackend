b86b5e4d1e2d1f23f76e4c010acadc81117d9e43
huzaifa8883 <aw3540341@gmail.com>
2026-05-21 00:05:59 +0500

    Add route to get enabled leagues by sport

--- src/routes/v1/settings.routes.js
+++ src/routes/v1/settings.routes.js
@@ -3,7 +3,8 @@ const { Router } = require('express');
 const ctrl = require('../../controllers/settings.controller');
 const router = Router();
 
-router.get('/leagues',  ctrl.getLeagues);
-router.post('/leagues', ctrl.saveLeagues);
+router.get('/leagues',              ctrl.getLeagues);
+router.post('/leagues',             ctrl.saveLeagues);
+router.get('/leagues/enabled/:sport', ctrl.getEnabledLeagues);  // ← NEW: dashboard use karega
 
 module.exports = router;
