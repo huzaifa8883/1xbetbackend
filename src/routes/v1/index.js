721c8db7205e48450e69c740ebc8374f13f65a9b
huzaifa8883 <aw3540341@gmail.com>
2026-05-20 23:45:53 +0500

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,10 +7,14 @@ const authRoutes    = require('./auth.routes');
 const orderRoutes   = require('./order.routes');
 const marketRoutes  = require('./market.routes');
 const userRoutes    = require('./user.routes');
+const settingsRoutes = require('./settings.routes');  // ← ADD
+
 
 router.use('/auth',    authRoutes);
 router.use('/orders',  orderRoutes);
 router.use('/markets', marketRoutes);
 router.use('/users',   userRoutes);
+router.use('/settings', settingsRoutes);              // ← ADD
+
 
 module.exports = router;
